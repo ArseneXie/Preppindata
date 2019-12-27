45e04491e97d08c8de7e43842c3c3b239c3263b9
Arsene Xie <ArseneXie@users.noreply.github.com>
2019-12-27 09:37:06 +0800

    2019W46

--- 2019W46/preppindataw46.R
+++ 2019W46/preppindataw46.R
@@ -16,50 +16,17 @@ join_list <- c('Nice List','Naughty List') %>%
   filter(mapply(grepl,`Name Part`,`Name`)) 
 
 detail_list <- join_list %>%
-  group_by()
+  group_by(`List`,`Name`,`Address`,`Family Role`,`Item`) %>%
+  summarise('Elves Build Time (min)'=max(`Elves Build Time (min)`),
+            'Reason'=last(`Reason`)) 
 
-  
+summary <- detail_list %>%
+  group_by(`List`) %>%
+  summarise('Total Hours Build Time'= round(sum(`Elves Build Time (min)`)/60)) 
 
+missing_list <- anti_join(read_excel(input, sheet = 'Present List'),join_list,
+                          by=c('Name','Address'='Address Part'))  
 
-all_list<- c('Nice List','Naughty List') %>%
-  set_names() %>%
-  map_df(~ read_excel(path = input, sheet = .x,),.id = 'List') %>%
-  rename('Name Part' = 'Name') 
-
-present <- read_excel(input, sheet = 'Present List') %>% 
-  rename('Address Part' = 'Address') 
-
-
-c <- regex_inner_join(all_list,present,by = c('Address'='Address Part')) %>%
-  merge(.,regex_inner_join(present,all_list,by = c('Name'='Name Part')),
-        by=c('List','Name','Address')) %>%
-  distinct() 
-
-join_list <- c('Nice List','Naughty List') %>%
-  set_names() %>%
-  map_df(~ read_excel(path = input, sheet = .x,),.id = 'List') %>%
-  regex_inner_join(read_excel(input, sheet = 'Present List'), 
-                   by = c('N'))
-  
-
-input <- "E:/2019 PD Wk 46.xlsx"
-
-final <-  input %>%
-  excel_sheets() %>%
-  set_names() %>%
-  map_df(
-    ~ read_excel(path = input, sheet = .x,),
-    .id = "sheet name") %>%
-
-final <- read_excel("E:/Week Two Input.xlsx", 
-                    col_types = c("text", "text", "text", 
-                                  "numeric", "date")) %>%
-  drop_na() %>%
-  mutate('City' = if_else(grepl('.*gh$',`City`),'Edinburgh','London')) %>%
-  unite('R2CHdr',`Metric`,`Measure`, sep=' - ') %>%
-  spread(`R2CHdr`,`Value`)
-
-View(final)
-
-final <-  c('Nice List','Naughty List') %>%
-  set_names() 
+View(summary)
+View(detail_list)
+View(missing_list)
