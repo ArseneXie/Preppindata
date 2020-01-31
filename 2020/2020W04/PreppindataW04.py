6cd3edd85a7f821fd0f24caf38e04a6984831436
Arsene Xie <ArseneXie@users.noreply.github.com>
2020-01-31 17:27:51 +0800

    Update PreppindataW04.py

--- 2020/2020W04/PreppindataW04.py
+++ 2020/2020W04/PreppindataW04.py
@@ -1,52 +1,49 @@
 import pandas as pd
 import re
-from datetime import datetime
 
-xls = pd.ExcelFile("E:/PD - NBA Results.xlsx")
-team_list = pd.read_excel(xls,"Team List").set_index('Team').T.to_dict('dict')
-def get_conf(tname):
-    return team_list[tname]['Conference']
+def clean_country(x):
+    return 'England' if re.search('(gland)',x,re.IGNORECASE) else \
+        'Scotland' if re.search('(^sc)',x,re.IGNORECASE) else \
+            'Netherlands' if re.search('(Netherland)',x,re.IGNORECASE) else \
+                'United States' if re.search('(United)',x,re.IGNORECASE) else x
+def date_ymd_str(x):
+    x = re.sub('\D','@', re.sub('(^.*)(?:Dec)(.*$)',r'\1/12/\2', re.sub('(^.*)(?:Jan)(.*$)',r'\1/01/\2', re.sub('\s','', re.sub('(.*\D$)',r'\1 2020',x)))))
+    return re.sub('(?:@*)(\d+)(?:@*)(\d+)(?:@*)(\d+)',r'\3/\2/\1',x) if re.search('(@*\d+@*((0?[1-9])|(1[0-2]))@*\d{4})',x) else \
+        re.sub('(?:@*)(\d+)(?:@*)(\d+)(?:@*)(\d+)',r'\3/\1/\2',x) if re.search('(@*((0?[1-9])|(1[0-2]))@*\d+@*\d{4})',x) else \
+            re.sub('(?:@*)(\d+)(?:@*)(\d+)(?:@*)(\d+)',r'\1/\2/\3',x) if re.search('(@*\d{4}@*((0?[1-9])|(1[0-2]))@*\d+)',x) else x
+def time_str(x):
+    dx = re.sub('\D','',x)
+    return str(int(re.search('(\d+)(?=\d{2})',dx).group(1))+(12 if re.search('(p|P)',x) else 0)).zfill(2)+':'+re.search('(\d{2}$)',dx).group(1)
+          
+result = pd.read_csv(r'E:/PD 2020 Wk 4 Input.csv' )
+question = pd.read_csv(r'E:/Store Survey Results - Question Sheet.csv' )
 
-temp = []
-for sheet in [sh for sh in xls.sheet_names if re.search('Results$',sh)]:
-    df = pd.read_excel(xls,sheet)
-    temp.append(df.rename(columns={df.columns[5]:'PTS_H'}))
-games = pd.concat(temp)[['Date','Visitor/Neutral','PTS','Home/Neutral','PTS_H']].dropna().copy()
-games['Date'] =  games['Date'].apply(lambda x: datetime.strptime(re.sub('^\w+\s','',x),'%b %d %Y'))
-games['Away Conf'] = games['Visitor/Neutral'].apply(lambda x: get_conf(x))
-games['Home Conf'] = games['Home/Neutral'].apply(lambda x: get_conf(x))
-games['In Conf'] = games.apply(lambda x: x['Away Conf']==x['Home Conf'], axis=1)
-games['Away'] =  games.apply(lambda x: [x['Away Conf'],x['Visitor/Neutral'],('W' if x['PTS']>x['PTS_H'] else 'L')], axis=1)
-games['Home'] =  games.apply(lambda x: [x['Home Conf'],x['Home/Neutral'],('W' if x['PTS_H']>x['PTS'] else 'L')], axis=1)
-games = games[['Date','In Conf','Away','Home']].copy()
-games = games.melt(id_vars=['Date','In Conf'],value_name='Team data', var_name='Away Home')
-games['Conference'] = games['Team data'].apply(lambda x: x[0])
-games['Team'] = games['Team data'].apply(lambda x: x[1])
-games['Win Lose'] = games['Team data'].apply(lambda x: x[2])
-games['Away check'] = games['Away Home'].apply(lambda x: 1 if x=='Away' else 0)
-games['Home check'] = games['Away Home'].apply(lambda x: 1 if x=='Home' else 0)
-games['Last N'] = games.groupby('Team')['Date'].rank(ascending=False).astype(int)
-games['L10 check'] = games['Last N'].apply(lambda x: 1 if x<=10 else 0)
-games['Conf check'] = games['In Conf'].apply(lambda x: 1 if x else 0)
+result['Id'] = result.groupby('Question Number').cumcount()+1
+result = pd.merge(result,question,how='inner',left_on='Question Number',right_on='Number')
+result.drop(['Question Number','Number',],axis=1,inplace=True)
+result['Country'] = result.apply(lambda x: x['Country'] if x['Country'] else x['Store'], axis=1)
+result['Country'] = result['Country'].apply(lambda x: clean_country(x))
+result['DoB'].fillna('',inplace=True)
 
-wl_team = games.groupby(['Conference','Team','Win Lose'],as_index=False).agg({'Date':'count'}).rename(columns={'Date':'Value'})
-wl_away = games.groupby(['Conference','Team','Win Lose'],as_index=False).agg({'Away check':'sum'}).rename(columns={'Away check':'Value'})
-wl_away['Win Lose'] = wl_away['Win Lose'].apply(lambda x: 'Away '+x)
-wl_home = games.groupby(['Conference','Team','Win Lose'],as_index=False).agg({'Home check':'sum'}).rename(columns={'Home check':'Value'})
-wl_home['Win Lose'] = wl_home['Win Lose'].apply(lambda x: 'Home '+x)
-wl_last = games.groupby(['Conference','Team','Win Lose'],as_index=False).agg({'L10 check':'sum'}).rename(columns={'L10 check':'Value'})
-wl_last['Win Lose'] = wl_last['Win Lose'].apply(lambda x: 'L10 '+x)
-wl_conf = games.groupby(['Conference','Team','Win Lose'],as_index=False).agg({'Conf check':'sum'}).rename(columns={'Conf check':'Value'})
-wl_conf['Win Lose'] = wl_conf['Win Lose'].apply(lambda x: 'Conf '+x)
-wl_strk = games.groupby(['Conference','Team','Win Lose'],as_index=False).agg({'Last N':'min'}).rename(columns={'Last N':'Value'})
-wl_strk['Win Lose'] = wl_strk['Win Lose'].apply(lambda x: 'Strk '+x)
+final = result.pivot_table(index=[c for c in result.columns if not re.match('Question|Answer',c)],
+                           columns='Question',values='Answer',aggfunc=max).reset_index()
+final['Date'] = final['What day did you fill the survey in?'].apply(lambda x: date_ymd_str(x))
+final['Time'] = final['What time did you fill the survey in?'].apply(lambda x: time_str(x))
+final['Completion Date'] = final.apply(lambda x: 
+                                       pd.to_datetime(x['Date'] + ' ' + ('00:00' if x['Time']=='24:00' else x['Time']), format='%Y/%m/%d %H:%M')
+                                       + pd.DateOffset(days=(1 if x['Time']=='24:00' else 0)),axis=1)
+final['Age of Customer'] = final['DoB'].apply(lambda x: 2020-int(re.search('(\d+$)',x).group(1)) if re.search('(\d+$)',x) else None)    
+final['OrderAsc'] = final.groupby(['Country','Store','Name'])['Completion Date'].rank().astype(int)
+final['OrderDesc'] = final.groupby(['Country','Store','Name'])['Completion Date'].rank(ascending=False).astype(int)
+final['Would you recommend C&BSco to your friends and family? (Score 0-10)']=final['Would you recommend C&BSco to your friends and family? (Score 0-10)'].astype(int)
+final['Result'] = final.apply(lambda x: 'First' if x['OrderAsc']==1 else 'Latest' if x['OrderDesc']==1 else 'Drop', axis=1)
+final['Detractor'] = final['Would you recommend C&BSco to your friends and family? (Score 0-10)'].apply(lambda x: 1 if x<=6 else None)
+final['Promoter'] = final['Would you recommend C&BSco to your friends and family? (Score 0-10)'].apply(lambda x: 1 if x>=9 else None)
+final['Passive'] = final['Would you recommend C&BSco to your friends and family? (Score 0-10)'].apply(lambda x: 1 if 6<x<9 else None)
+final = final.query("Result!='Drop'")[['Country', 'Store', 'Name','Completion Date','Result',
+                                        'Would you recommend C&BSco to your friends and family? (Score 0-10)',
+                                        'Promoter','Detractor','Passive',
+                                        'Age of Customer','If you would, why?',"If you wouldn't, why?"]].copy()
 
-final = pd.concat([wl_team, wl_conf,  wl_home, wl_away, wl_last, wl_strk], sort=False, ignore_index=True)
-final = final.pivot_table(index=['Conference','Team'], columns='Win Lose', values='Value', aggfunc=max).reset_index()
-for cate in ['Conf','Home','Away','L10']:
-    final[cate] = final.apply(lambda x: str(x[cate+' W'])+'-'+str(x[cate+' L']), axis=1)
-final['Pct'] = round(final['W']/(final['L']+final['W']),3)
-final['Strk'] = final.apply(lambda x: 'W'+str(x['Strk L']-1) if x['Strk W']==1 else 'L'+str(x['Strk W']-1), axis=1)
-final['Rank'] = final.groupby('Conference')['Pct'].rank(ascending=False,method='max').astype(int)
-final_east = final.query("Conference=='Eastern'")[['Rank','Team','W','L','Pct','Conf','Home','Away','L10','Strk']].sort_values(by='Rank')
-final_west = final.query("Conference=='Western'")[['Rank','Team','W','L','Pct','Conf','Home','Away','L10','Strk']].sort_values(by='Rank') 
+
+
