4fa84018cf1c1771cecc4587ea57dd1f8a7fee47
Arsene Xie <ArseneXie@users.noreply.github.com>
2020-04-17 13:35:37 +0800

    2020W16

--- 2020/2020W16/preppindataw16.R
+++ 2020/2020W16/preppindataw16.R
@@ -1,31 +1,25 @@
 library(dplyr)
 library(readxl)
-library(splitstackshape)
-library(arules)
+library(tidyr)
+library(purrr)
 library(stringr)
 
-items <- read_excel('E:/Transactions.xlsx', sheet = 1) %>%
-  select('Items') %>%
-  cSplit_e(., 'Items', ',', drop = TRUE, type = 'character', fill=0) %>%
-  rename_all(.funs = funs(sub('Items_','', .)))
+input <- "E:/Can't Desktop Prep this-2.xlsx"
 
-rules <- apriori(as.matrix(items), parameter = list(supp = 0.001, conf = 0.01))
-
-sup <- inspect(subset(rules, subset = size(lhs)==0)) %>%
-  select(c('rhs','support')) %>% 
-  mutate('lhs'=`rhs`,
-         'LHS Support'=`support`,
-         'RHS Support'=`support`) %>%
-  select(-'support') 
-
-final <- inspect(subset(rules, subset = size(lhs)==1 & size(rhs)==1)) %>%
-  select(c('lhs','rhs','confidence','lift')) %>%
-  merge(.,sup %>% select(c('rhs','RHS Support')), by='rhs') %>%
-  merge(.,sup %>% select(c('lhs','LHS Support')), by='lhs') %>%
-  mutate_if(is.factor, as.character) %>%
-  mutate('LHS Item' = str_extract(`lhs`,'(?<=\\{)(.*)(?=\\})'),
-         'RHS Item' = str_extract(`rhs`,'(?<=\\{)(.*)(?=\\})'),
-         'Association Rule' = paste(`LHS Item`,'-->',`RHS Item`)) %>%
-  select(c('Association Rule','LHS Item', 'RHS Item', 'LHS Support', 'RHS Support','confidence', 'lift'))
+final <- input %>%
+  excel_sheets() %>% .[grepl('.*Sales$',.)] %>%
+  set_names() %>%
+  map_df(
+    ~ read_excel(path = input, sheet = .x),
+    .id = 'Store') %>%
+  mutate('Store' = str_extract(`Store`,'(.*)(?=\\s)')) %>%
+  pivot_longer(., cols=matches('^(Sales|Profit)'), names_to='Col', values_to='Value') %>%
+  separate(`Col`,c('Measure','Date'), sep=' ') %>%
+  mutate('Date' = as.Date(`Date`, format='%d/%m/%Y')) %>%
+  pivot_wider(., names_from=`Measure`, values_from=`Value`) %>%
+  merge(., read_excel(input, 'Staff days worked') %>%
+          pivot_longer(., cols=matches('[^(Month)]'), names_to='Store', values_to='Staff days worked'),
+        by.x=c('Store','Date'), by.y=c('Store','Month')) %>%
+  select(c('Store', 'Category', 'Scent', 'Date', 'Sales', 'Profit', 'Staff days worked'))
 
 View(final)
