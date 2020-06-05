d52f49eadea3e23feef39104857e46185f56fc96
Arsene Xie <ArseneXie@users.noreply.github.com>
2020-06-05 18:01:17 +0800

    temp

--- 2020/2020W22/preppindataw22.R
+++ 2020/2020W22/preppindataw22.R
@@ -1,36 +1,35 @@
 library(dplyr)
-library(readr)
+library(readxl)
 library(tidyr)
-library(scales)
+library(stringr)
 
-df <- read_csv("F:/Data/2020W21 Input.csv") %>%
-  mutate('Group' = if_else(`Company`=='Chin & Beard Suds Co','C&BSCo','Rest of Market'))
+xlsx <- "F:/Data/2020W22 Input.xlsx"
 
-finalA <- df %>% group_by(`Company`,`Month`) %>%
-  mutate('CM Total' = sum(`Sales`)) %>%
-  group_by(`Month`) %>%
-  mutate('Mon Total' = sum(`Sales`)) %>%
-  pivot_longer(.,cols=matches('Total$'), names_to='Measure', values_to='R2CVal') %>%
-  unite('R2CCol','Month','Measure',sep=' ') %>%
-  pivot_wider(.,id_cols='Company', names_from='R2CCol', values_from='R2CVal',values_fn = list(R2CVal = max)) %>%
-  mutate('Growth' = label_percent(0.01)((`April CM Total`-`March CM Total`)/`March CM Total`),
-         'April Market Share' = round(`April CM Total`/`April Mon Total`*100,2),
-         'Bps Change' = round((`April CM Total`/`April Mon Total`-`March CM Total`/`March Mon Total`)/0.0001)) %>%
-  select(c('Company', 'Growth','April Market Share', 'Bps Change'))
+ttl <- read_excel(xlsx, sheet = "Total Market") 
+comp <- read_excel(xlsx, sheet = "Companies") 
+scent <- read_excel(xlsx, sheet = "Scents") 
+
+march_ttl <- ttl$'March Sales'[1]
+april_ttl <- march_ttl*(1+ttl$'Growth'[1])
 
 
-finalB <- df %>% group_by(`Soap Scent`,`Group`,`Month`) %>%
-  mutate('SGM Total' = sum(`Sales`)) %>%
-  group_by(`Group`,`Month`) %>%
-  mutate('GM Total' = sum(`Sales`)) %>%
-  pivot_longer(.,cols=matches('Total$'), names_to='Measure', values_to='R2CVal') %>%
-  unite('R2CCol','Month','Measure',sep=' ') %>%
-  pivot_wider(.,id_cols=c('Soap Scent','Group'), names_from='R2CCol', values_from='R2CVal',values_fn = list(R2CVal = max)) %>%
-  mutate('R2CCol' = paste(`Group`,'Contribution to Growth'),
-         'R2CVal' = round((`April SGM Total`-`March SGM Total`)/`March GM Total`*100,2)) %>%
-  pivot_wider(.,id_cols=c('Soap Scent'), names_from='R2CCol', values_from='R2CVal',values_fn = list(R2CVal = max)) %>%
-  mutate('Outperformance' = `C&BSCo Contribution to Growth`-`Rest of Market Contribution to Growth`) %>%
-  select(c('Soap Scent','C&BSCo Contribution to Growth','Rest of Market Contribution to Growth','Outperformance'))
+comp['random1'] = comp.apply(lambda x:random.random(),axis=1)
+comp['random2'] = comp.apply(lambda x:random.random(),axis=1)
+comp['March'] = march_ttl*comp['random1']/comp['random1'].sum()
+comp['randomRank'] = comp['random2'].rank().astype(int)
+comp['ChangeBps'] = comp['randomRank'].apply(lambda x: -30+x*10)
+comp['April'] = april_ttl*(comp['random1']/comp['random1'].sum()+comp['ChangeBps']/10000)
 
-View(finalA)
-View(finalB)
+
+final <- comp %>%
+  mutate('random1' = str_extract(`Store`,'(.*)(?=\\s)')) %>%
+  pivot_longer(., cols=matches('^(Sales|Profit)'), names_to='Col', values_to='Value') %>%
+  separate(`Col`,c('Measure','Date'), sep=' ') %>%
+  mutate('Date' = as.Date(`Date`, format='%d/%m/%Y')) %>%
+  pivot_wider(., names_from=`Measure`, values_from=`Value`) %>%
+  merge(., read_excel(input, 'Staff days worked') %>%
+          pivot_longer(., cols=matches('[^(Month)]'), names_to='Store', values_to='Staff days worked'),
+        by.x=c('Store','Date'), by.y=c('Store','Month')) %>%
+  select(c('Store', 'Category', 'Scent', 'Date', 'Sales', 'Profit', 'Staff days worked'))
+
+View(final)
