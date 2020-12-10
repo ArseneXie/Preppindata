7ee66730b402e486f672d3f596bd1530696131c4
Arsene Xie <ArseneXie@users.noreply.github.com>
2020-12-10 18:02:56 +0800

    temp

--- 2020/2020W49/PreppindataW49.py
+++ 2020/2020W49/PreppindataW49.py
@@ -1,16 +1,17 @@
 import pandas as pd
+import re
 
-xlsx = pd.ExcelFile("F:/Data/2020W47 Input.xlsx")
+xlsx = pd.ExcelFile("F:/Data/NBA 2018_19 Results.xlsx")
 
-delays = pd.read_excel(xlsx,'Delays')
-delays[['Airport','Type']] = delays[['Airport','Type']].ffill()
-delays = delays.dropna().replace({'Airport':{'JKF':'JFK'}})
-delays = delays.groupby(['Airport','Type'], as_index=False).agg({'RecordID':'count', 'Delay':'sum'})
-delays = delays.rename(columns={'RecordID':'Delay flights'})
-
-final = pd.merge(pd.read_excel(xlsx,'On Time'), delays, on=['Airport','Type'])
-final['% Flights Delayed'] = round(final['Delay flights']/(final['Delay flights']+final['Number of flights '])*100,2)
-final['Avg Delay (mins)'] = round(final['Delay']/(final['Delay flights']+final['Number of flights ']),2)
-final = final[['Airport', 'Type', '% Flights Delayed', 'Avg Delay (mins)']]
-
-#python rounding half to even.
+temp = []
+for sheet in xlsx.sheet_names:
+    df = pd.read_excel(xlsx,sheet)
+    temp.append(df)
+game = pd.concat(temp)[[c for c in df.columns if re.match('Date|.*Neutral|PTS.*',c)]] 
+game.columns = ['Date', 'Visitor', 'Visitor PTS', 'Home', 'Home PTS']
+final = game.melt(id_vars= ['Date','Visitor PTS','Home PTS'], var_name='Type', value_name='Team')
+final['Win'] = final.apply(lambda x: 1 if x[x['Type']+' PTS']==max(x['Visitor PTS'],x['Home PTS']) else 0,axis=1)
+final['Game Number per Team'] = final.groupby(['Team'])['Date'].rank().astype(int)
+final['Win'] = final.sort_values('Date').groupby(['Team'])['Win'].cumsum()
+final['Rank'] = final.sort_values(['Win','Team'], ascending=[False,True]).groupby('Game Number per Team').cumcount()+1
+final = final[['Rank','Win','Team', 'Game Number per Team']].copy()
