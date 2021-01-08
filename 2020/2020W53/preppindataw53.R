5fd4ae89c8db6fddbc5c7e8d23416ccf803d03e4
Arsene Xie <ArseneXie@users.noreply.github.com>
2021-01-09 00:28:13 +0800

    2020W53

--- 2020/2020W53/preppindataw53.R
+++ 2020/2020W53/preppindataw53.R
@@ -2,37 +2,43 @@ library(readr)
 library(dplyr)
 library(tidyr)
 library(stringr)
+library(lubridate)
+library(fuzzyjoin)
 
-old_star <- read_csv("E:/Old Star Signs.csv", col_names = FALSE)
+Sys.setlocale("LC_ALL","English")
+
+old_star <- read_csv("F:/Data/Old Star Signs.csv", col_names = FALSE)[1:2] %>%
+  `colnames<-`(c('Old Star Sign', 'DateRange')) %>%
+  rbind(.,read_csv("F:/Data/Old Star Signs.csv", col_names = FALSE)[3:4] %>%
+          `colnames<-`(c('Old Star Sign', 'DateRange'))) %>%
+  rbind(.,read_csv("F:/Data/Old Star Signs.csv", col_names = FALSE)[5:6] %>%
+          `colnames<-`(c('Old Star Sign', 'DateRange'))) %>%
+  drop_na() %>%
+  mutate('Begin Date' = as.Date(paste0('2020/',str_extract(`DateRange`, '^(\\d+/\\d+)'))),
+         'End Date' = as.Date(paste0('2020/',str_extract(`DateRange`, '(\\d+/\\d+)$'))),
+         'End Date' =  `End Date` %m+% years(ifelse(`End Date`<`Begin Date`,1,0)))
+
+new_star <- read_csv("F:/Data/New Star Signs.csv", col_names = FALSE) %>%
+  mutate('New Star Sign' = str_extract(`X1`, '^(\\w+)'),
+         'From Date' = as.Date(paste0('2020 ',str_replace(`X1`, '.*:\\s(\\w{3})\\w*\\s(\\d+).*','\\1 \\2')),"%Y %b %d"),
+         'To Date' = as.Date(paste0('2020 ',str_replace(`X1`, '.*\\s(\\w{3})\\w*\\s(\\d+)$','\\1 \\2')),"%Y %b %d"),
+         'To Date' =  `To Date` %m+% years(ifelse(`To Date`<`From Date`,1,0)),
+         'Date Range' = paste(str_replace(`X1`, '.*:\\s(\\w{3})\\w*\\s(\\d+).*','\\2 \\1'),'-',
+                              str_replace(`X1`, '.*\\s(\\w{3})\\w*\\s(\\d+)$','\\2 \\1'))) 
+
+all_date = read_csv("F:/Data/Scaffold.csv", col_types = cols(Date = col_date(format = "%d/%m/%Y"))) %>%
+  mutate('Birthday' = format(`Date`,'%b %d'))
 
 
-finalA <- read_excel("F:/Data/USOpenWinners.xlsx") %>%
-  rename_all(str_to_title) %>%
-  pivot_longer(., col=starts_with('Round'), names_to='Round Num', values_to='Round Score') %>%
-  mutate('Round Par' = round((`Total`- as.integer(ifelse(`To Par`=='E',0,`To Par`)))/4),
-         'Round to Par' = `Round Score` - `Round Par`,
-         'Round Colors' = case_when(`Round Num`=='Round 1'~'A', `Round Num`=='Round 2'~'B', `Round Num`=='Round 3'~'C', TRUE ~ 'D'),
-         'Point1' = case_when(`Round Num`=='Round 1'~'1,0', `Round Num`=='Round 2'~'1,-1', `Round Num`=='Round 3'~'0,-1', TRUE ~ '0,0'),
-         'Point2' = case_when(`Round Num`=='Round 1'~'0,0', `Round Num`=='Round 2'~'0,-1', `Round Num`=='Round 3'~'-1,-1', TRUE ~ '-1,0'),
-         'Point3' = case_when(`Round Num`=='Round 1'~'0,1', `Round Num`=='Round 2'~'0,0', `Round Num`=='Round 3'~'-1,0', TRUE ~ '-1,1'),
-         'Point4' = case_when(`Round Num`=='Round 1'~'1,1', `Round Num`=='Round 2'~'1,0', `Round Num`=='Round 3'~'0,0', TRUE ~ '0,1')) %>%
-  pivot_longer(., col=starts_with('Point'), names_to='Point', values_to='Coordinate') %>%
-  merge(.,read_excel("F:/Data/Location Prize Money.xlsx") %>% select(c('Year', 'Country', 'Venue', 'Location')), by='Year') %>%
-  separate(`Coordinate`, c('X Coordinate Polygon', 'Y Coordinate Polygon'), sep =',', convert = TRUE) %>%
-  mutate('X Coordinate Polygon' = `X Coordinate Polygon`*sqrt(`Round Score`),
-         'Y Coordinate Polygon' = `X Coordinate Polygon`*sqrt(`Round Score`),
-         'Decade' = as.integer(`Year`/10)*10,
-         'Row' = as.integer((`Decade` - min(`Decade`))/10+1),
-         'Column' = `Year` - `Decade` + 1) %>%
-  select(-c('Pos', 'To Par'))
+final <- merge(fuzzy_inner_join(all_date, old_star, 
+                                by = c('Date'='Begin Date','Date'='End Date'), match_fun = list(`>=`, `<=`)),
+               fuzzy_inner_join(all_date, new_star, 
+                                by = c('Date'='From Date','Date'='To Date'), match_fun = list(`>=`, `<=`)),
+               by='Birthday') %>%
+  group_by(`Birthday`) %>%
+  mutate('The Same' = ifelse(`Old Star Sign`==`New Star Sign`,1,0),
+         'The Same Cases' = max(`The Same`)) %>%
+  filter(`The Same Cases`==0) %>%
+  select(c('Birthday', 'Old Star Sign', 'New Star Sign', 'Date Range'))
 
-
-finalB <- finalA %>%
-  group_by(`Decade`) %>%
-  summarise('Min Round Score' = min(`Round Score`),
-            'Max Round Score' = max(`Round Score`),
-            'Min Total Score' = min(`Total`),
-            'Max Total Score' = max(`Total`), .groups='drop')
-
-View(finalA)
-View(finalB)
+View(final)
