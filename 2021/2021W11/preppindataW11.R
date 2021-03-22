28b7855b13e073e730cd087a5b80a0c404518c25
Arsene Xie <ArseneXie@users.noreply.github.com>
2021-03-22 10:51:16 +0800

    2021W11

--- 2021/2021W11/preppindataW11.R
+++ 2021/2021W11/preppindataW11.R
@@ -1,46 +1,22 @@
 library(readxl)
 library(dplyr)
+library(tidyr)
+library(splitstackshape)
 library(stringr)
-library(data.tree) 
 
-xlsx <- "F:/Data/Pokemon Input.xlsx"
-  
-pokemon <- read_excel(xlsx, sheet = "Pokemon") %>%
-  filter(!str_detect(`Name`,'^Mega\\s')) %>%
-  filter(as.numeric(str_trim(`#`))<=386) %>%
-  select(-'Type') %>%
-  distinct()
+xlsx <- "F:/Data/Cocktails Dataset.xlsx"
 
-evolution <- read_excel(xlsx, sheet = "Evolution") %>%
-  distinct() %>%
-  filter(`Evolving to` %in% pokemon$Name) 
-
-rule <- evolution %>%
-  select(c('Evolving from', 'Evolving to')) %>%
-  filter(!(`Evolving from` %in% evolution$`Evolving to`)) %>%
-  mutate('Evolving to'=`Evolving from`,
-         'Evolving from'="root2") %>%
-  rbind(evolution %>%
-          select(c('Evolving from', 'Evolving to')) ,.) %>%
-  FromDataFrameNetwork(.)
-
-evo_grp <- ToDataFrameTree(rule, 
-                           Name =  function(x) x$path[x$level],
-                           `Evolution Group` = function(x) x$path[2])[-1,-1]
-
-final <- merge(pokemon,evolution, by.x='Name', by.y='Evolving from', all.x=TRUE) %>%
-  merge(., evolution %>% select(c('Evolving from', 'Evolving to')), by.x='Name', by.y='Evolving to', all.x=TRUE) %>%
-  merge(., evo_grp, by='Name', all.x=TRUE) %>%
-  mutate('Evolution Group' = coalesce(`Evolution Group`,`Name`)) %>%
-  select(c('Evolution Group', '#', 'Name', 'Total', 'HP', 'Attack', 'Defense', 'Special Attack', 
-           'Special Defense', 'Speed', 'Evolving from', 'Evolving to', 'Level', 'Condition', 'Evolution Type'))
+final <- read_excel(xlsx, sheet = "Cocktails") %>%
+  cSplit(., 'Recipe (ml)', ';') %>%
+  pivot_longer(starts_with('Recipe'), names_to = 'ToDrop', values_to='Recipe', values_drop_na=TRUE) %>%
+  separate(`Recipe`, c('Ingredient','Measurement'), sep=':' ) %>%
+  merge(read_excel(xlsx, sheet = "Sourcing") %>%
+          merge(read_excel(xlsx, sheet = "Conversion Rates"), by='Currency'), by='Ingredient') %>%
+  group_by(`Cocktail`) %>%
+  summarise('Cost' = round(sum(as.numeric(str_extract(`Measurement`,'(\\d+)'))*`Price`
+                               /`ml per Bottle`/`Conversion Rate ¢G`),2),
+            'Price' = max(`Price (¢G)`),
+            'Margin' = `Price` - `Cost`, .groups='drop') %>%
+  select(c('Cocktail', 'Price', 'Cost', 'Margin'))
 
 View(final)
-
-#---
-#rule_tree<- ToDataFrameTree(rule, 
-#                            level1 = function(x) x$path[2],
-#                            level2 = function(x) x$path[3],
-#                            level3 = function(x) x$path[4],
-#                            level_number = function(x) x$level - 1)[-1,-1]
-#---
