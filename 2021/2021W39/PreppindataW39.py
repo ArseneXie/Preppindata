e1cf31bebc6619ab5598a9229f94961bbcc88dce
Arsene Xie <zhxiang.xie@gmail.com>
2021-10-18 15:48:12 +0800

    2021W39

--- 2021/2021W39/PreppindataW39.py
+++ 2021/2021W39/PreppindataW39.py
@@ -1,14 +1,19 @@
 import pandas as pd
+import re
 
-lift = pd.read_csv("F:\\Data\\2021W30.csv")
-lift = lift.sort_values(['Hour', 'Minute'], ascending=[True, True]).reset_index(drop=True)
-lift = lift.replace({'From':{'G':0,'B':-1}, 'To':{'G':0,'B':-1}})
-lift['From'] = lift['From'].astype('int')
-lift['To'] = lift['To'].astype('int')
-lift['Floors Btw Trips'] = abs(lift['From'] - lift['To'].shift(1))
-lift['Default position'] = lift.groupby('From').count().Hour.idxmax()
-lift['From Default Position'] = abs(lift['From'] - lift['Default position'])
+process = pd.read_csv("C:\\Data\\PreppinData\\Bike Painting Process - Painting Process.csv")
+process['Datetime'] = process.apply(lambda x: pd.to_datetime(f"{x['Date']} {x['Time']}"), axis=1)
+process = process.groupby(['Batch No.']).apply(pd.DataFrame.sort_values, 'Datetime', ascending=True).reset_index(drop=True)
+ 
+for attr in ['Bike Type', 'Batch Status', 'Name of Process Stage']:
+    process[attr] = process.apply(lambda x: x['Data Value'] if x['Data Parameter']==attr else None, axis=1)
+    process[attr] = process.groupby(['Batch No.'])[attr].ffill() 
+    
+process = process[(process['Data Type']=='Process Data') & (process['Data Parameter']!='Name of Process Stage')]
+for vtype in ['Actual', 'Target']:
+    process[vtype] = process.apply(lambda x: float(x['Data Value']) if re.match(f'^({vtype})',x['Data Parameter']) else None, axis=1)
+process['Data Parameter'] = process['Data Parameter'].apply(lambda x: re.sub('^(\w+\s)','',x))
+process = process[['Batch No.', 'Bike Type', 'Batch Status', 'Name of Process Stage',
+                   'Data Parameter', 'Actual', 'Target', 'Datetime']]
+   
 
-final = lift.groupby('Default position', as_index=False).agg({'From Default Position':'mean', 'Floors Btw Trips':'mean'})
-final['Difference'] = final['From Default Position'] - final['Floors Btw Trips'] 
-final = final.rename(columns={'From Default Position':'Avg travel from default position', 'Floors Btw Trips':'Avg travel between trips currently'})
--- 2021/2021W39/preppindataW39.R
+++ 2021/2021W39/preppindataW39.R
@@ -1,23 +1,21 @@
 library(readr)
 library(dplyr)
+library(tidyr)
+library(stringr)
 
-get_floor <- function(floor_str){
-  return(as.integer(if_else(floor_str=='G', '0', if_else(floor_str=='B', '-1',floor_str))))
-}
-
-final <- read_csv("F:/Data/2021W30.csv") %>%
-  arrange(`Hour`,`Minute`) %>%
-  mutate('From'= get_floor(`From`),
-         'To'= get_floor(`To`),
-         'Floors Btw Trips' = abs(lag(`To`)-`From`)) %>%
-  group_by(`From`) %>%
-  mutate('FromCount' = n()) %>%
-  ungroup() %>%
-  mutate('Default Position' = .[[max(which(`FromCount`== max(`FromCount`))),'From']],
-         'From Default Position' = abs(`Default Position`-`From`)) %>%
-  group_by(`Default Position`) %>%
-  summarise('Avg travel from default position' = mean(`From Default Position`),
-            'Avg travel between trips currently' = mean(`Floors Btw Trips`, na.rm=TRUE),
-            'Difference' = `Avg travel from default position` - `Avg travel between trips currently`)
+final <- read_csv("C:/Data/PreppinData/Bike Painting Process - Painting Process.csv") %>%
+  mutate('Datetime' = as.POSIXct(paste(`Date`, `Time`),format='%d/%m/%Y %H:%M:%S'),
+         'Bike Type' = if_else(`Data Parameter`=='Bike Type', `Data Value`, NA_character_),
+         'Batch Status' = if_else(`Data Parameter`=='Batch Status', `Data Value`, NA_character_),
+         'Name of Process Stage' = if_else(`Data Parameter`=='Name of Process Stage', `Data Value`, NA_character_)) %>%
+  group_by(`Batch No.`) %>%
+  arrange(`Datetime`) %>%
+  fill(`Bike Type`, `Batch Status`, `Name of Process Stage`) %>%
+  filter((`Data Type`=='Process Data') & (`Data Parameter`!='Name of Process Stage')) %>%
+  mutate('Actual' = if_else(str_detect(`Data Parameter`, '^(Actual)'), as.numeric(`Data Value`), NA_real_),
+         'Target' = if_else(str_detect(`Data Parameter`, '^(Target)'), as.numeric(`Data Value`), NA_real_),
+         'Data Parameter' = str_remove(`Data Parameter`,'^(\\w+\\s)')) %>%
+  select(c('Batch No.', 'Bike Type', 'Batch Status', 'Name of Process Stage',
+           'Data Parameter', 'Actual', 'Target', 'Datetime'))
 
 View(final)
