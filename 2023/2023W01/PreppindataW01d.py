ed250a8758fbc56763a1e8a73ed40f20250a85d4
Arsene Xie <zhxiang.xie@gmail.com>
2023-05-22 12:16:12 +0800

    fix

--- 2023/2023W01/PreppindataW01d.py
+++ 2023/2023W01/PreppindataW01d.py
@@ -5,7 +5,7 @@ conn = duckdb.connect()
 conn.execute("""
 CREATE TEMP TABLE txn_detail AS        
 SELECT split_part("Transaction Code",'-',1) Bank, dayname("Transaction Date") "Transaction Date",
-     CASE "Online or In-Person" WHEN 2 THEN 'Online' ELSE 'In-Person' END "Online or In-Person",
+     CASE "Online or In-Person" WHEN 1 THEN 'Online' ELSE 'In-Person' END "Online or In-Person",
      "Customer Code", "Value" 
      FROM read_csv_auto('C:/Data/PreppinData/PD 2023 Wk 1 Input.csv', header=True)
 """)
--- 2023/2023W01/preppindataW01.R
+++ 2023/2023W01/preppindataW01.R
@@ -7,7 +7,7 @@ Sys.setlocale("LC_ALL","English")
 txndata <- read_csv("C:/Data/PreppinData/PD 2023 Wk 1 Input.csv", 
                   col_types = cols(`Transaction Date` = col_date(format = "%d/%m/%Y %H:%M:%S"))) %>%
   mutate('Bank' = str_extract(`Transaction Code`,'^([A-Z]+)'),
-         'Online or In-Person' = if_else(`Online or In-Person`==1, 'In-Person', 'Online'),
+         'Online or In-Person' = if_else(`Online or In-Person`==1, 'Online', 'In-Person'),
          'Transaction Date' = strftime(`Transaction Date`, '%A'))
 
 out1 <- txndata %>%
